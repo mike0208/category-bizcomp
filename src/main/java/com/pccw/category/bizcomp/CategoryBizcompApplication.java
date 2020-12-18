773e52e9353cdb75ca06f72a53074ce122b9af20
mayank <mayank.gwal@ltts.com>
2020-12-18 14:09:03 +0530

    initial commit

---
+++ src/main/java/com/pccw/category/bizcomp/CategoryBizcompApplication.java
@@ -0,0 +1,13 @@
+package com.pccw.category.bizcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryBizcompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryBizcompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pccw/categorybizcomp/CategoryBizcompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pccw.categorybizcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryBizcompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
